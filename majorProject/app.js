9a987cdb3a954428cca227aa5fb9926ab53de36f
= <salimtauheed2002@gmail.com>
2024-09-28 19:06:55 +0530

    day1 of major project

--- majorProject/app.js
+++ majorProject/app.js
@@ -3,13 +3,14 @@ const app =  express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
-
+const methodOverride =  require('method-override');
 //setting up ejs
 app.set("view engine", "ejs");
 
 app.set("views", __dirname + "/views");          //app.set("views", path.join(__dirname, "views")); since __dirname is a node.js variable that returns the address of the current directory in which the app.js is present
 //to parse(encode) the url
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 //creating and connecting database happyHaven 
 
@@ -33,6 +34,12 @@ app.get("/listings", async (req, res)=>{
     res.render("listings/allListings.ejs", {allListings});  
 })
 
+
+//note: put new route above the show route bcs "/listings/:id" is confusing with "/listings/new"
+//new route: rout to render a form to create a new listing
+app.get("/listings/new", (req, res)=>{
+    res.render("listings/newListing.ejs");
+})
 //show route: to show an individual listing
 
 app.get("/listings/:id", async (req, res)=>{
@@ -42,12 +49,43 @@ app.get("/listings/:id", async (req, res)=>{
 })
 
 
-  
+//routr to save the entered listing info
+
+  app.post("/listings", async (req, res)=>{
+    //let {title, description, price, location, country} =  req.body; //this is also a way to get the information from post request
+    // console.log(req.body.listing)
+    let listing = new Listing(req.body.listing);
+    //console.log(listing);
+   await listing.save();
+   res.redirect("/listings");
+  })
+
+//route to render the edit form
+app.get("/listings/:id/edit",async (req, res)=>{
+    let {_id} = req.params;
+    let listing = await Listing.findById("66f7e4153be96a5563b30525");
+    //console.log(listing); 
+    res.render("listings/edit.ejs", {listing});
+});
+
+//update route
+
+app.put("/listings/:id", async (req, res)=>{
+    let {id} = req.params;
+   // console.log(req.body.listing);
+  //  console.log(await Listing.findById(id));
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
 
 
+//delete route
 
-
-
+app.delete("/listings/:id", async (req, res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings"); 
+})
 
 
 
