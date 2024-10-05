f02d08d24056a66149b8a9a12249f0ec4b16d223
= <salimtauheed2002@gmail.com>
2024-10-05 18:47:48 +0530

    major project day 7 added

--- majorProject/app.js
+++ majorProject/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const app =  express();
+const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
-const methodOverride =  require('method-override');
+const wrapAsync = require('./utils/wrapAsync.js')
+const methodOverride = require('method-override');
 //setting up ejs-mate. ejs-mate is basically used as the include 
 const ejsMate = require('ejs-mate');
 app.engine("ejs", ejsMate);
@@ -14,105 +15,99 @@ app.set("view engine", "ejs");
 
 app.set("views", __dirname + "/views");          //app.set("views", path.join(__dirname, "views")); since __dirname is a node.js variable that returns the address of the current directory in which the app.js is present
 //to parse(encode) the url
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 //creating and connecting database happyHaven 
 
-async function main(){
+async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/happyhaven');
 }
 
-main().then(()=>{})
-.catch((err)=>{
-    console.log(err);
-})
+main().then(() => { })
+    .catch((err) => {
+        console.log(err);
+    })
 
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("hello guys how are you");
 });
 
 //index route to show all the data
-app.get("/listings", async (req, res)=>{
+app.get("/listings", async (req, res) => {
     let allListings = await Listing.find({});
-    res.render("listings/allListings.ejs", {allListings});  
+    res.render("listings/allListings.ejs", { allListings });
 })
 
 
 //note: put new route above the show route bcs "/listings/:id" is confusing with "/listings/new"
 //new route: rout to render a form to create a new listing
-app.get("/listings/new", (req, res)=>{
+app.get("/listings/new", (req, res) => {
     res.render("listings/newListing.ejs");
 })
 //show route: to show an individual listing
 
-app.get("/listings/:id", async (req, res)=>{
-    let {id} = req.params;
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
     let listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
+    res.render("listings/show.ejs", { listing });
 })
 
 
-//routr to save the entered listing info
+//create route: routr to save the entered listing info
 
-  app.post("/listings", async (req, res)=>{
-    //let {title, description, price, location, country} =  req.body; //this is also a way to get the information from post request
-    // console.log(req.body.listing)
-    let listing = new Listing(req.body.listing);
-    //console.log(listing);
-   await listing.save();
-   res.redirect("/listings");
-  })
- 
+
+
+app.post("/listings",wrapAsync (async (req, res, next) => {
+   
+        //let {title, description, price, location, country} =  req.body; //this is also a way to get the information from post request
+        // console.log(req.body.listing)
+        let listing = new Listing(req.body.listing);
+        //console.log(listing);
+        await listing.save();
+        res.redirect("/listings");
+
+})); 
+
+
+
+
 //route to render the edit form
-app.get("/listings/:id/edit",async (req, res)=>{
-    let {id} = req.params;
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
     let listing = await Listing.findById(id);
     //console.log(listing); 
-    res.render("listings/edit.ejs", {listing});
+    res.render("listings/edit.ejs", { listing });
 });
 
 //update route
 
-app.put("/listings/:id", async (req, res)=>{
-    let {id} = req.params;
-   // console.log(req.body.listing);
-  //  console.log(await Listing.findById(id));
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+app.put("/listings/:id", wrapAsync( async (req, res) => {
+    let { id } = req.params;
+    // console.log(req.body.listing);
+    //  console.log(await Listing.findById(id));
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
-})
+}));
 
 
 //delete route
 
-app.delete("/listings/:id", async (req, res)=>{
-    let {id} = req.params;
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
     await Listing.findByIdAndDelete(id);
-    res.redirect("/listings"); 
+    res.redirect("/listings");
 })
 
 
 
 
-/*
-//database testing route
-app.get("/testingdb", async (req, res)=>{
-    let l1 = new Listing({
-        title: "Calm haven for stay",
-        description:"Stay with beautiful nature",
-        price: 6000,
-        location: "Hugai",
-        country: "Singapur"
-    });
+//middleware to handle error for any route
+app.use((err, req, res, next) => {
+    res.send("Something went wrong")  //it will take care of error at server side before saving the documents in the database like validation error, constraints etc
+})
 
-    await l1.save()
-    console.log("listing is saved");
-    res.send("successfull testing");
-});
-*/
-
-
-app.listen(8080, (req, res)=>{
+app.listen(8080, (req, res) => {
     console.log("i am listening");
 })
