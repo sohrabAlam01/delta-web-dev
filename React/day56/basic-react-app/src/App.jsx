30b228a8649166ad076eb68c216186d6ede6e7ff
= <salimtauheed2002@gmail.com>
2024-11-16 04:39:43 +0530

    react day1 added

---
+++ React/day56/basic-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+import {Title1, Title2} from "./Title.jsx"
+import ProductTab from './ProductTab.jsx';
+
+function App() {
+
+let name = "Muskan";
+  return (
+    <>
+      {/* <Title1/>
+      <Title2/>
+      <p>Hi, {name}</p>
+      <p>Hi, {name.toUpperCase()}</p>
+      <p> 2*2 = {2*2}</p> */}
+      <ProductTab></ProductTab>
+
+    </>
+  )
+
+}
+
+export default App
---
+++ React/day56/basic-react-app/src/Title.jsx
@@ -0,0 +1,11 @@
+function Title1(){
+
+    return <h1>I am Title1</h1>
+  };
+
+function Title2(){
+    return <h1>I am Title2</h1>
+}
+
+// export default Title1;
+  export {Title1, Title2};
