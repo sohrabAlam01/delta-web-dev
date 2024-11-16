c9c9583d9aa783a425647e9f8ebf2f06313bc718
= <salimtauheed2002@gmail.com>
2024-11-17 05:09:50 +0530

    react day2

--- React/day56/basic-react-app/src/Product.jsx
+++ React/day56/basic-react-app/src/Product.jsx
@@ -1,16 +1,41 @@
 import "./Product.css"
 
-function Product(){
+// since props is an object 
+// function Product(props){       
+
+//     return (
+//        <div className="Product">
+//                    <h2>{props.title}</h2>
+//                    <h4>{props.description}</h4>
+//                      <h4>{props.price}</h4>
+//        </div>
+
+
+//     )
+
+// }
+
+// another way to get title and description 
+function Product({ title, description, price = 1, features, features2, features3, features4 }) {
 
     return (
-       <div className="Product">
-                   <h2>Product Title</h2>
-                   <h4>Product Description</h4>
-       </div>
-
         
+        <div className="Product">
+            <h2>{title}</h2>
+            <h4>{description}</h4>
+            <h4>{price}</h4>
+            <p>{features}</p>
+            <p>
+               {features3.map((feature) => (
+                <li>{feature}</li>
+            ))}
+            </p>
+        </div>
+
+
     )
 
 }
+
 
 export default Product;
--- React/day56/basic-react-app/src/ProductTab.jsx
+++ React/day56/basic-react-app/src/ProductTab.jsx
@@ -1,15 +1,27 @@
 import Product from "./Product.jsx"
 
-function ProductTab(){
+function ProductTab() {
 
-    return (
+  //passsing array and object as props
+  let options = [<li>duearion</li>, <li>Hi-tech</li>, <li>best</li>];
+  let options2 = { a: "Durable", b: "Hi-tech", c: "Best sellor" };
 
-        <>
-          <Product></Product>
-          <Product></Product>
-          <Product></Product>
-        </>
-    )
+  return (
+
+    <>
+      <Product title="Phone"
+       description="This is a good phone"
+        price={1000} features={options} 
+        features2={options2} 
+        features3={["Durable", "Luxury"]} 
+        features4={{ a: "Durable", b: "Costly" }}
+      ></Product>
+      <Product title="Laptop" description="This is a good Laptop" price={500} features2={options2}></Product>
+
+      {/* //we can write this way */}
+      <Product title="Phone" description="This is a good phone" />
+    </>
+  )
 }
 
-export default ProductTab;
+export default ProductTab;    
